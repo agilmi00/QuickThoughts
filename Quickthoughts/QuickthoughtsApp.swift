124a77af42620752088ec20dc1d8a86c488ebe7e
Cl0ud7 <agilmi00@estudiantes.unileon.es>
2022-05-31 11:37:54 +0200

    Initial Commit

---
+++ Quickthoughts/QuickthoughtsApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuickthoughtsApp.swift
+//  Quickthoughts
+//
+//  Created by Adrián Gil Miranda on 31/5/22.
+//
+
+import SwiftUI
+
+@main
+struct QuickthoughtsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
